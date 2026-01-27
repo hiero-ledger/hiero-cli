fa87f342fa98fab27641e689f534c018b925fafd
matevszm <mateusz.marcinkowski@blockydevs.com>
2026-01-27 15:22:20 +0100

    feat(1205): Update dependencies
    Signed-off-by: matevszm <mateusz.marcinkowski@blockydevs.com>

--- jest.config.js
+++ jest.config.js
@@ -11,10 +11,20 @@ module.exports = {
         },
       },
     ],
+    '^.+\\.js$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+        useESM: true,
+      },
+    ],
   },
   testEnvironment: 'node',
   testTimeout: 40000,
   testPathIgnorePatterns: ['.*/__tests__/helpers/.*'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(ansi-escapes|supports-hyperlinks|environment|supports-color|has-flag)/)',
+  ],
   reporters: ['default', 'jest-junit'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
