09108e06512cc1735a045b28604c91c3e724a9f9
rozekmichal <michal.rozek@blockydevs.com>
2026-01-29 13:35:29 +0100

    feat(1232): add erc20 symbol command
    Signed-off-by: rozekmichal <michal.rozek@blockydevs.com>

--- src/plugins/contract-erc20/index.ts
+++ src/plugins/contract-erc20/index.ts
@@ -3,4 +3,5 @@
  * Exports the config plugin manifest
  */
 export { name } from './commands/name';
+export { symbol } from './commands/symbol';
 export { contractErc20PluginManifest } from './manifest';
