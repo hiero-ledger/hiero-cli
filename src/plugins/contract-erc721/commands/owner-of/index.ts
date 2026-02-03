56034959048a21009d6f8771947f03380137d780
mmyslblocky <michal.myslinski@blockydevs.com>
2026-02-03 17:35:15 +0100

    feat(1243): added mocks
    Signed-off-by: mmyslblocky <michal.myslinski@blockydevs.com>

---
+++ src/plugins/contract-erc721/commands/owner-of/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Contract ownerOf Command Exports
+ * For use by tests and external consumers
+ */
+export { ownerOfFunctionCall } from './handler';
+export type { ContractErc721CallOwnerOfOutput } from './output';
+export {
+  CONTRACT_ERC721_CALL_OWNER_OF_TEMPLATE,
+  ContractErc721CallOwnerOfOutputSchema,
+} from './output';
--- src/plugins/contract-erc721/index.ts
+++ src/plugins/contract-erc721/index.ts
@@ -3,4 +3,5 @@
  * Exports the config plugin manifest
  */
 export { balanceOfFunctionCall } from './commands/balance-of';
+export { ownerOfFunctionCall } from './commands/owner-of';
 export { contractErc721PluginManifest } from './manifest';
