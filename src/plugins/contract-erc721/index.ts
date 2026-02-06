5a91367630d37bf7c05daed4ac63b4dcd910a78a
mmyslblocky <michal.myslinski@blockydevs.com>
2026-02-06 09:49:52 +0100

    feat(1251): safe-transfer-from erc721 function
    Signed-off-by: mmyslblocky <michal.myslinski@blockydevs.com>

--- src/plugins/contract-erc721/index.ts
+++ src/plugins/contract-erc721/index.ts
@@ -5,6 +5,7 @@
 export { balanceOfFunctionCall } from './commands/balance-of';
 export { nameFunctionCall } from './commands/name';
 export { ownerOfFunctionCall } from './commands/owner-of';
+export { safeTransferFromFunctionCall } from './commands/safe-transfer-from';
 export { setApprovalForAllFunctionCall } from './commands/set-approval-for-all';
 export { symbolFunctionCall } from './commands/symbol';
 export { tokenUriFunctionCall } from './commands/token-uri';
