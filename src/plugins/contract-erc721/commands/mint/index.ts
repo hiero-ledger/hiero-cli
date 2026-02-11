381ce86e52357577db4406f452f241af74037bce
rozekmichal <michal.rozek@blockydevs.com>
2026-02-11 11:37:44 +0100

    feat: add mint command for erc-721
    Signed-off-by: rozekmichal <michal.rozek@blockydevs.com>

---
+++ src/plugins/contract-erc721/commands/mint/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Contract function mint Command Exports
+ */
+export { mintFunctionCall } from './handler';
+export type { ContractErc721CallMintOutput } from './output';
+export {
+  CONTRACT_ERC721_CALL_MINT_TEMPLATE,
+  ContractErc721CallMintOutputSchema,
+} from './output';
