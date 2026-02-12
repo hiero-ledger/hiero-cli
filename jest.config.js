8dfc05cbe9338bae20756e94b45d77cd80a93338
rozekmichal <michal.rozek@blockydevs.com>
2026-02-12 13:36:56 +0100

    fix: ansi-escapes package bump reverted
    Signed-off-by: rozekmichal <michal.rozek@blockydevs.com>

--- jest.config.js
+++ jest.config.js
@@ -11,20 +11,10 @@ module.exports = {
         },
       },
     ],
-    '^.+\\.js$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.test.json',
-        useESM: true,
-      },
-    ],
   },
   testEnvironment: 'node',
   testTimeout: 40000,
   testPathIgnorePatterns: ['.*/__tests__/helpers/.*'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(ansi-escapes|supports-hyperlinks|environment|supports-color|has-flag)/)',
-  ],
   reporters: ['default', 'jest-junit'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
