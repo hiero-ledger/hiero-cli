57110b2d9179bd652c3a0fae1775e1223d67b7fc
matevszm <mateusz.marcinkowski@blockydevs.com>
2026-02-17 09:49:32 +0100

    feat(1313): Implement core error infrastructure (#1366)
    Signed-off-by: matevszm <mateusz.marcinkowski@blockydevs.com>

---
+++ src/core/errors/authorization-error.ts
@@ -0,0 +1,17 @@
+import { CliError } from './cli-error';
+
+export class AuthorizationError extends CliError {
+  static readonly CODE = 'AUTHORIZATION_ERROR';
+
+  constructor(
+    message: string,
+    options?: { context?: Record<string, unknown>; cause?: unknown },
+  ) {
+    super({
+      code: AuthorizationError.CODE,
+      message,
+      recoverable: false,
+      ...options,
+    });
+  }
+}
