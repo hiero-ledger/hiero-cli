0f6f037fdb05e2e9bac887ec46307ffe8802653a
matevszm <mateusz.marcinkowski@blockydevs.com>
2026-03-11 14:20:13 +0100

    fix(1549): fix integration tests
    Signed-off-by: matevszm <mateusz.marcinkowski@blockydevs.com>

--- src/plugins/account/index.ts
+++ src/plugins/account/index.ts
@@ -1,8 +1,11 @@
-export { AccountBalanceCommand } from './commands/balance/handler';
-export { ClearAccountsCommand } from './commands/clear/handler';
-export { CreateAccountCommand } from './commands/create/handler';
-export { DeleteAccountCommand } from './commands/delete/handler';
-export { ImportAccountCommand } from './commands/import/handler';
-export { ListAccountsCommand } from './commands/list/handler';
-export { ViewAccountCommand } from './commands/view/handler';
+export {
+  AccountBalanceCommand,
+  getAccountBalance,
+} from './commands/balance/handler';
+export { clearAccounts, ClearAccountsCommand } from './commands/clear/handler';
+export { createAccount, CreateAccountCommand } from './commands/create/handler';
+export { deleteAccount, DeleteAccountCommand } from './commands/delete/handler';
+export { importAccount, ImportAccountCommand } from './commands/import/handler';
+export { listAccounts, ListAccountsCommand } from './commands/list/handler';
+export { viewAccount, ViewAccountCommand } from './commands/view/handler';
 export { accountPluginManifest } from './manifest';
