b37dd9d5a7bf18cf70825c51b3eab692e96c3c63
rozekmichal <michal.rozek@blockydevs.com>
2026-03-17 11:58:15 +0100

    fix: PrivateKeySchema changed to KeySchema
    Signed-off-by: rozekmichal <michal.rozek@blockydevs.com>

--- src/plugins/topic/commands/create/input.ts
+++ src/plugins/topic/commands/create/input.ts
@@ -4,7 +4,6 @@ import {
   KeyManagerTypeSchema,
   KeySchema,
   MemoSchema,
-  PrivateKeySchema,
   TopicNameSchema,
 } from '@/core/schemas';
 
@@ -15,11 +14,11 @@ import {
 export const TopicCreateInputSchema = z.object({
   memo: MemoSchema.describe('Optional memo for the topic'),
   adminKey: z
-    .array(PrivateKeySchema)
+    .array(KeySchema)
     .optional()
     .default([])
     .describe(
-      'Admin key(s). Pass multiple times for multiple keys. Format: {accountId}:{privateKey}, private key in {ed25519|ecdsa}:private:{key} format, key reference or account alias',
+      'Admin key(s). Pass multiple times for multiple keys. Format: {accountId}:{privateKey}, public/private key in {ed25519|ecdsa}:public|private:{key} format, key reference or account alias',
     ),
   submitKey: z
     .array(KeySchema)
