a24adbd11fef1292d7a60a6e9e4dda3546f2d007
mmyslblocky <michal.myslinski@blockydevs.com>
2026-03-19 14:22:28 +0100

    feat(1627): fix template render by providing length function
    Signed-off-by: mmyslblocky <michal.myslinski@blockydevs.com>

--- src/core/errors/network-error.ts
+++ src/core/errors/network-error.ts
@@ -1,17 +1,15 @@
 import { CliError } from './cli-error';
 
-export const DEFAULT_TEMPLATE = '{{{message}}}';
-
 export class NetworkError extends CliError {
   private static readonly CODE = 'NETWORK_ERROR';
   private static readonly TEMPLATE = `
-  {{{message}}}\n
-  {{#if context.apiMessages}}{{#if (gt (length context.apiMessages) 0)}}
-  Detailed message:\n
-  {{#each context.apiMessages}}  
-  • {{this}}\n{{/each}}
-  {{/if}}{{/if}}
-  `.trim();
+{{message}}
+{{#if context.apiMessages}}{{#if (gt (length context.apiMessages) 0)}}
+Detailed error message:
+{{#each context.apiMessages}}  
+  • {{this}}{{/each}}
+{{/if}}{{/if}}
+`.trim();
 
   constructor(
     message: string,
