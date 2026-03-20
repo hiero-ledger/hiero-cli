3659cd40a743bef2d05f60b11f9bdc5e4c996264
rozekmichal <michal.rozek@blockydevs.com>
2026-03-20 11:32:46 +0100

    feat: consts moved to be static readonly inside a class
    Signed-off-by: rozekmichal <michal.rozek@blockydevs.com>

--- src/core/errors/transaction-error.ts
+++ src/core/errors/transaction-error.ts
@@ -1,14 +1,13 @@
 import { CliError } from './cli-error';
 
-const TRANSACTION_ERROR_TEMPLATE_WITH_LINK = `Transaction execution failed
+export class TransactionError extends CliError {
+  static readonly CODE = 'TRANSACTION_ERROR';
+  static readonly TEMPLATE_WITH_LINK = `Transaction execution failed
 Transaction ID: {{hashscanLink context.transactionId "transaction" context.network}}
 {{#if context.detailedMessage}}Detailed error message: {{context.detailedMessage}}{{/if}}`;
 
-const TRANSACTION_ERROR_TEMPLATE_DEFAULT = `{{{message}}}
+  static readonly TEMPLATE_DEFAULT = `{{{message}}}
 {{#if context.detailedMessage}}Detailed error message: {{context.detailedMessage}}{{/if}}`;
-
-export class TransactionError extends CliError {
-  static readonly CODE = 'TRANSACTION_ERROR';
 
   constructor(
     message: string,
@@ -25,7 +24,7 @@ export class TransactionError extends CliError {
 
   override getTemplate(): string {
     return this.context?.transactionId && this.context?.network
-      ? TRANSACTION_ERROR_TEMPLATE_WITH_LINK
-      : TRANSACTION_ERROR_TEMPLATE_DEFAULT;
+      ? TransactionError.TEMPLATE_WITH_LINK
+      : TransactionError.TEMPLATE_DEFAULT;
   }
 }
