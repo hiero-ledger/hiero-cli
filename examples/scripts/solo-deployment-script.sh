df27c517d244e96a1ddd824b771c3782e864e39d
mmyslblocky <michal.myslinski@blockydevs.com>
2026-03-23 15:38:28 +0100

    feat(1619): updated workflow and added script
    Signed-off-by: mmyslblocky <michal.myslinski@blockydevs.com>

--- examples/scripts/solo-deployment-script.sh
+++ examples/scripts/solo-deployment-script.sh
@@ -1,11 +1,32 @@
+#!/usr/bin/env bash
+# Non-interactive shells don't load ~/.bashrc, so system `node` (e.g. v12) wins unless we source nvm.
+
+#Install kubectl
+ARCH="$(dpkg --print-architecture)"
+curl -fsSLo kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${ARCH}/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+
+#Install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+#Install solo
+npm install -g @hashgraph/solo@latest
+
+rm -rf ~/.solo
+
 export SOLO_CLUSTER_NAME=solo
 export SOLO_NAMESPACE=solo
 export SOLO_CLUSTER_SETUP_NAMESPACE=solo-cluster
 export SOLO_DEPLOYMENT=solo-deployment
 
 kind get clusters | grep '^solo' | while read cluster; do   kind delete cluster -n "$cluster"; done
+echo "Deleted all solo clusters"
+
 solo one-shot single deploy
-mirror-1-rest-5bc89d94c8-7vbml
+echo "Deployed solo cluster"
 
 # Find the pod with prefix mirror-1-rest across all namespaces
 RESULT=$(kubectl get pods --all-namespaces --no-headers -o custom-columns=":metadata.namespace,:metadata.name" | grep '^.* mirror-1-rest' | head -n1)
@@ -24,5 +45,5 @@ POD=$(echo $RESULT | awk '{print $2}')
 echo "Forwarding port 5551:5551 to pod $POD in namespace $NAMESPACE"
 kubectl port-forward -n $NAMESPACE pod/$POD 5551:5551 >/dev/null 2>&1 &
 
-pkill -f "kubectl port-forward.*mirror-1-rest"
-solo one-shot single destroy
+#pkill -f "kubectl port-forward.*mirror-1-rest"
+#solo one-shot single destroy
