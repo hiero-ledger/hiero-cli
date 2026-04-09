6d7045bcc23aba642cc70c50f64f66ec45cd8aa3
matevszm <mateusz.marcinkowski@blockydevs.com>
2026-04-09 11:27:24 +0200

    feat(1770): Unit test optimization
    Signed-off-by: matevszm <mateusz.marcinkowski@blockydevs.com>

---
+++ src/core/shared/config/default-plugin-names.ts
@@ -0,0 +1,14 @@
+export const DEFAULT_PLUGIN_NAMES = new Set([
+  'account',
+  'batch',
+  'token',
+  'network',
+  'plugin-management',
+  'credentials',
+  'topic',
+  'hbar',
+  'contract',
+  'config',
+  'contract-erc20',
+  'contract-erc721',
+]);
